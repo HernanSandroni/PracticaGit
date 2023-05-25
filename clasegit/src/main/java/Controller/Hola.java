70e1e5bc0d3098d632f84defc06d5f5b87ce6ba3
HernanSandroni <hernasandroni@gmail.com>
2023-05-25 16:06:22 -0300

    Added endpoint Hola. Todo: fix endpoint

---
+++ clasegit/src/main/java/Controller/Hola.java
@@ -0,0 +1,15 @@
+package Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/hola")
+public class Hola {
+    @GetMapping
+    public String getHola(){
+        return "hola coders!";
+    }
+}
